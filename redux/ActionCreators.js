4c4a0dfb31b9710f30d68789f3ac7cef8e02bfc3
Krosis855 <jtspalding22@gmail.com>
2021-08-01 11:39:16 -0700

    Swipe and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -183,3 +183,8 @@ export const addFavorite = campsiteId => ({
     payload: campsiteId
 });
 
+export const deleteFavorite = campsiteId => ({
+  type: ActionTypes.DELETE_FAVORITE,
+  payload: campsiteId
+})
+
